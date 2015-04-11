9a26bc6492e54fc6ee4ade379684cd9f5cb5fe4e
pjhjohn <pjhjohn@gmail.com>
2015-04-11 23:35:11 +0900

    화면 전체를 덮는 NavigationDrawer 완성

--- app/src/main/java/com/montserrat/controller/ApplicationManager.java
+++ app/src/main/java/com/montserrat/controller/ApplicationManager.java
@@ -1,15 +1,33 @@
 package com.montserrat.controller;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by pjhjohn on 2015-04-10.
  * Contains Various Parameters for the application : Should be singleton.
  */
 public class ApplicationManager {
-    private ApplicationManager() {}
+    private static String APP_PREFERENCE_NAME = "montserrat_shared_preference";
+    private ApplicationManager () {
+        this.appContext = null;
+        this.sharedpref = null;
+    }
     private static ApplicationManager instance;
     public static ApplicationManager getInstance() {
         if ( instance == null ) {
             return instance = new ApplicationManager();
         } else return instance;
+    }
+
+    private Context appContext;
+    private void setApplicationContext(Context context) {
+        this.appContext = context;
+        this.sharedpref = this.appContext.getSharedPreferences(APP_PREFERENCE_NAME, Context.MODE_PRIVATE);
+    }
+
+    private SharedPreferences sharedpref;
+    private SharedPreferences getSharedPreference() {
+        return this.sharedpref;
     }
 }
