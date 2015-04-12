83d89c9debf58d0453727b62acbf3ba746d1f435
pjhjohn <pjhjohn@gmail.com>
2015-04-12 22:00:10 +0900

    교수 갤러리 더미 추가

---
+++ app/src/main/java/com/montserrat/utils/samples/SampleClientFragmentWithListView.java
@@ -0,0 +1,89 @@
+package com.montserrat.utils.samples;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+
+import com.montserrat.activity.R;
+import com.montserrat.utils.request.ClientFragmentWithListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class SampleClientFragmentWithListView extends ClientFragmentWithListView<SampleUniversalItemView> {
+    public SampleClientFragmentWithListView () {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+
+        /* Set ItemClick Listener for Action */
+        this.listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick (AdapterView<?> parent, View view, int position, long id) {
+                // Do Something
+            }
+        });
+
+        /* Request for Data */
+        this.submit(new JSONObject());
+
+        return view;
+    }
+
+    /* TODO : FILL IT. It's necessary. */
+    @Override
+    protected int getFragmentLayoutId () {
+        return R.layout.main_fragment;
+    }
+
+    @Override
+    protected String getEndpoint () {
+        return "http://pjhjohn.appspot.com/search";
+    }
+
+    @Override
+    protected int getListViewId () {
+        return R.id.main_listview;
+    }
+
+    /* TODO : Fill if necessary */
+    @Override
+    protected int getProgressViewId() {
+        return 0;
+    }
+
+    @Override
+    protected int getContentViewId() {
+        return 0;
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        super.onResponse(response);
+        try {
+            if(response.getBoolean("success")) {
+                JSONArray data = response.getJSONArray("data");
+                for (int i = 0; i < data.length(); i++) {
+                    JSONObject row = (JSONObject) data.get(i);
+                    this.items.add(new SampleUniversalItemView(new SampleUniversalItemView.Data(
+                            row.getString("subject"),
+                            row.getString("professor"),
+                            (float)row.getDouble("rating")
+                    )));
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        this.adapter.notifyDataSetChanged();
+    }
+
+    public static Fragment newInstance (int i) {
+        return new SampleClientFragmentWithListView();
+    }
+}
