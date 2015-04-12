f374455ac5bc21b50e52380d23879d4480544b96
pjhjohn <pjhjohn@gmail.com>
2015-04-12 22:59:58 +0900

    RecycleView 적용 시작

--- app/src/main/java/com/montserrat/parts/main/MainFragment.java
+++ app/src/main/java/com/montserrat/parts/main/MainFragment.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ListView;
 
 import com.melnykov.fab.FloatingActionButton;
 import com.montserrat.activity.R;
