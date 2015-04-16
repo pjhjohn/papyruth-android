0bf9d23dad4db50c5ea5bfbb31c429241b3f4d49
pjhjohn <pjhjohn@gmail.com>
2015-04-16 20:57:58 +0900

    AppConst.java : 어플리케이션 번들 키와 디폴트 값 설정

---
+++ app/src/main/java/com/montserrat/controller/AppConst.java
@@ -0,0 +1,20 @@
+package com.montserrat.controller;
+
+/**
+ * Created by pjhjohn on 2015-04-16.
+ */
+public class AppConst {
+    public static class Resource {
+        public static final int DEFAULT = 0;
+        public static final String PROGRESS = "view_progress";
+        public static final String CONTENT = "view_content";
+        public static final String FRAGMENT = "view_fragment";
+        public static final String SWIPE_REFRESH = "view_swipe_refresh";
+    }
+    public static class Request {
+        public static final String DEFAULT = "";
+        public static final String URL = "url";
+        public static final String CONTROLLER = "controller";
+        public static final String ACTION = "action";
+    }
+}
