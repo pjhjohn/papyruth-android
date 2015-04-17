991c5e22e817d30cfdfaf00f4f6d9bdf69fe5920
pjhjohn <pjhjohn@gmail.com>
2015-04-17 14:25:16 +0900

    AppConst 네이밍 컨벤션대로 맞춤 + User KEY-STRING 추가

--- app/src/main/java/com/montserrat/controller/AppConst.java
+++ app/src/main/java/com/montserrat/controller/AppConst.java
@@ -7,25 +7,25 @@ public class AppConst {
     public static class Resource {
         /* For ClientFragment */
         public static final int DEFAULT = 0;
-        public static final String PROGRESS = "view_progress";
-        public static final String CONTENT = "view_content";
-        public static final String FRAGMENT = "view_fragment";
+        public static final String PROGRESS = "resource_progress";
+        public static final String CONTENT = "resource_content";
+        public static final String FRAGMENT = "resource_fragment";
 
         /* For ClientFragment with ListView */
-        public static final String LIST = "view_list";
+        public static final String LIST = "resource_list";
 
         /* For ClientFragment with RecyclerView */
-        public static final String TOOLBAR = "view_toolbar";
-        public static final String FAB = "view_fab";
-        public static final String SWIPE_REFRESH = "view_swipe_refresh";
-        public static final String RECYCLER = "view_recycler";
+        public static final String TOOLBAR = "resource_toolbar";
+        public static final String FAB = "resource_fab";
+        public static final String SWIPE_REFRESH = "resource_swipe_refresh";
+        public static final String RECYCLER = "resource_recycler";
     }
     public static class Request {
         /* For ClientFragment - Volley */
         public static final String DEFAULT = "";
-        public static final String URL = "url";
-        public static final String CONTROLLER = "controller";
-        public static final String ACTION = "action";
+        public static final String URL = "request_url";
+        public static final String CONTROLLER = "request_controller";
+        public static final String ACTION = "request_action";
     }
     public static class ViewPager {
         public static class Auth {
@@ -37,5 +37,12 @@ public class AppConst {
         public static class Main {
 
         }
+    }
+    public static class User {
+        public static final String UNIVERSITY = "user_university";
+        public static final String EMAIL = "user_email";
+        public static final String NAME = "user_name";
+        public static final String GENDER = "user_gender";
+        public static final String AUTO_SIGNIN = "user_auto_signin";
     }
 }
