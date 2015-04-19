189d20c88f35a8b8f5f54777ba707b035f4ff097
pjhjohn <pjhjohn@gmail.com>
2015-04-19 23:16:30 +0900

    유저정보 컨테이너 클래스 업데이트

--- app/src/main/java/com/montserrat/parts/auth/UserInfo.java
+++ app/src/main/java/com/montserrat/parts/auth/UserInfo.java
@@ -11,31 +11,34 @@ public class UserInfo {
         return ourInstance;
     }
 
-    private UserInfo () {
-        this.name = null;
-        this.email = null;
-        this.school = null;
-        this.isBoy = null;
-        this.validUser = false;
-    }
-
     private CharSequence name;
+    private CharSequence nickname;
     private CharSequence email;
     private Integer school;
+    private Integer admission;
     private Boolean isBoy;
     private boolean validUser;
 
-    public boolean isUserSigningUp() {
-        return this.validUser;
+    private UserInfo () {
+        this.clear();
     }
 
     public void clear() {
-        this.name = null;
-        this.email = null;
-        this.school = null;
-        this.isBoy = null;
+        this.name       = null;
+        this.nickname   = null;
+        this.email      = null;
+        this.school     = null;
+        this.isBoy      = null;
+        this.admission  = null;
+        this.validUser  = false;
+    }
+    public boolean isUserValid () {
+        return this.validUser;
     }
 
+    public void setNickName(CharSequence nickname) {
+        this.nickname = nickname;
+    }
     public void setEmail(CharSequence email) {
         this.email = email;
     }
@@ -45,6 +48,12 @@ public class UserInfo {
     public void setSchool(int schoolCode) {
         this.school = schoolCode;
     }
+    public void setAdmissionYear(int admissionYear) {
+        this.admission = admissionYear;
+    }
+    public void setGender(boolean isBoy) {
+        this.isBoy = isBoy;
+    }
 
     public boolean isReadyToSubmitForFirstStep() {
         return this.school != null;
@@ -53,7 +62,9 @@ public class UserInfo {
     public boolean isReadyToSubmitForSecondStep() {
         return this.school != null
                 && this.name != null
+                && this.nickname != null
                 && this.email != null
-                && this.isBoy != null;
+                && this.isBoy != null
+                && this.admission != null;
     }
 }
