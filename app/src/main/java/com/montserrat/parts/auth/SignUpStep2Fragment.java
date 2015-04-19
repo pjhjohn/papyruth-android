e91be7ea9b34b675300c9cc54cef407aca96d507
pjhjohn <pjhjohn@gmail.com>
2015-04-19 23:06:03 +0900

    회원정보기입 페이지 레이아웃

--- app/src/main/java/com/montserrat/parts/auth/SignUpStep2Fragment.java
+++ app/src/main/java/com/montserrat/parts/auth/SignUpStep2Fragment.java
@@ -2,15 +2,21 @@ package com.montserrat.parts.auth;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+import com.montserrat.activity.MainActivity;
 import com.montserrat.activity.R;
 import com.montserrat.controller.AppConst;
 import com.montserrat.utils.request.ClientFragment;
 import com.montserrat.utils.viewpager.ViewPagerController;
+
+import java.util.ArrayList;
 
 /**
  * Created by pjhjohn on 2015-04-12.
@@ -31,9 +37,15 @@ public class SignUpStep2Fragment extends ClientFragment {
         view.findViewById(R.id.btn_signup_submit).setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                SignUpStep2Fragment.this.pageController.setCurrentPage(AppConst.ViewPager.Auth.SIGNUP_STEP3);
+//                SignUpStep2Fragment.this.pageController.setCurrentPage(AppConst.ViewPager.Auth.SIGNUP_STEP3);
+                SignUpStep2Fragment.this.getActivity().startActivity(new Intent(SignUpStep2Fragment.this.getActivity(), MainActivity.class));
             }
         });
+
+        Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
+        ArrayList<String> list = new ArrayList<String>();
+        for(int i = 0; i < 50; i ++) list.add(""+(2015-i));
+        spinner.setAdapter(new ArrayAdapter<String>(this.getActivity(), R.layout.spinner_item_year, list));
         return view;
     }
 
