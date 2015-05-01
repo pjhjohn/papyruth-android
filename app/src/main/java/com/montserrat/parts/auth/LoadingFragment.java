a4dd75dd46e3208d6c4b05f37ecd68af5b312457
pjhjohn <pjhjohn@gmail.com>
2015-05-01 20:20:07 +0900

    LoadingFragment에서 RxAndroid로 Volley 테스트 성공

--- app/src/main/java/com/montserrat/parts/auth/LoadingFragment.java
+++ app/src/main/java/com/montserrat/parts/auth/LoadingFragment.java
@@ -11,6 +11,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,13 +23,21 @@ import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.RequestFuture;
 import com.montserrat.activity.MainActivity;
 import com.montserrat.activity.R;
 import com.montserrat.controller.AppConst;
 import com.montserrat.controller.AppManager;
 import com.montserrat.utils.request.ClientFragment;
+import com.montserrat.utils.request.RequestQueue;
 import com.montserrat.utils.validator.Validator;
 import com.montserrat.utils.viewpager.ViewPagerController;
 
@@ -36,77 +45,110 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action0;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+
+import static android.widget.Toast.LENGTH_LONG;
 
 /**
  * Created by pjhjohn on 2015-04-12.
  */
 
 // TODO : TIMER for minimum loading period
-public class LoadingFragment extends ClientFragment {
+public class LoadingFragment extends Fragment {
+    /* Setup ViewPagerController */
     private ViewPagerController pagerController;
-    private boolean isTimerTerminated;
-    private boolean isResourceDownloaded;
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         this.pagerController = (ViewPagerController) activity;
     }
-
-    private TextView vUnivText, vUserText, vEvalText;
-    private ImageView vUnivIcon, vUserIcon, vEvalIcon;
-
+    /* Inflate Fragment View */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = super.onCreateView(inflater, container, savedInstanceState);
-
-        /* Bind Views */
-        vUnivText = (TextView) view.findViewById(R.id.loading_university_text);
-        vUserText = (TextView) view.findViewById(R.id.loading_users_text);
-        vEvalText = (TextView) view.findViewById(R.id.loading_evaluations_text);
-        vUnivIcon = (ImageView) view.findViewById(R.id.loading_university_image);
-        vUserIcon = (ImageView) view.findViewById(R.id.loading_users_image);
-        vEvalIcon = (ImageView) view.findViewById(R.id.loading_evaluations_image);
-
-        /* Set Listeners */
-        return view;
+        return inflater.inflate(R.layout.fragment_loading, container, false);
     }
 
+    /*----------------------  RxAndroid  ----------------------*/
+
+    Subscription _subscription;
+    /* Subscribe */
     @Override
-    public void onStart() {
-        super.onStart();
-        String access_token = AppManager.getInstance().getString(AppConst.Preference.ACCESS_TOKEN, null);
-        if (access_token == null) {
-            // TODO : no access-token -> retrieve data from '/info' -> wait for it -> to AuthFragment
-        } else {
-            // TODO : has access-token -> retrieve data from '/universities/<university_id> -> wait for it -> to HomeFragment
-        }
+    public void onResume() {
+        super.onResume();
+        _subscription = observableJsonObjectRequest(new JSONObject(), Request.Method.GET, "http://mont.izz.kr:3001/api/v1/info").subscribe(JsonObjectRequestObserver());
+    }
+    /* Unsubscribe */
+    @Override
+    public void onPause() {
+        super.onPause();
+        _subscription.unsubscribe();
     }
 
-    @Override
-    public void onResponse(JSONObject response) {
-        super.onResponse(response);
-        AppManager.getInstance().getBoolean(AppConst.Preference.AUTO_SIGNIN, false);
+    /* Observable */
+    public Observable<JSONObject> observableJsonObjectRequest(JSONObject body, int method, String url) {
+        return Observable.create(subscription -> {
+            RequestQueue.getInstance(this.getActivity()).addToRequestQueue(new JsonObjectRequest(
+                method,
+                url,
+                body,
+                subscription::onNext,
+                subscription::onError
+            ){
+                @Override
+                public Map<String, String> getHeaders () {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("Authorization", String.format("Token token = %s", AppManager.getInstance().getString(AppConst.Preference.ACCESS_TOKEN, "")));
+                    return params;
+                }
+            });
+        });
     }
 
-    @Override
-    public void onErrorResponse(VolleyError error) {
-        super.onErrorResponse(error);
-        if(error.networkResponse.statusCode == 401) {
-            // TODO : access-token has expired -> retrieve data from '/info' -> wait for it -> to AuthFragment
-        } else {
-            // TODO : Specify error type - if no connection -> Fetch data from SharedPreference
-        }
+    /* Observer */
+    public Observer<JSONObject> JsonObjectRequestObserver() {
+        return new Observer<JSONObject>() {
+            @Override
+            public void onCompleted () {
+                Log.e("DEBUG", "onCompleted");
+            }
+
+            @Override
+            public void onError (Throwable e) {
+                Log.e("DEBUG", "onError");
+                VolleyError ve = (VolleyError) e;
+                if(ve.networkResponse.statusCode == 401) onStatus401(ve);
+                else if(ve.networkResponse.statusCode == 403) onStatus403(ve);
+            }
+
+            @Override
+            public void onNext (JSONObject response) {
+                Log.e("DEBUG", "Response : " + response);
+            }
+        };
+    }
+
+    public void onStatus401(VolleyError e) {
+        Log.e("DEBUG", "Status 401 : Not Authorized");
+    }
+    public void onStatus403(VolleyError e ) {
+        Log.e("DEBUG", "Status 403 : Forbidden ");
     }
 
     public static Fragment newInstance() {
-        Fragment fragment = new LoadingFragment();
-        Bundle bundle = new Bundle();
-        bundle.putString(AppConst.Request.API_ROOT_URL, AppConst.API_ROOT);
-        bundle.putString(AppConst.Request.API_VERSION, AppConst.API_VERSION);
-        bundle.putInt(AppConst.Resource.FRAGMENT, R.layout.fragment_loading);
-        fragment.setArguments(bundle);
-        return fragment;
+        return new LoadingFragment();
     }
 }
 
