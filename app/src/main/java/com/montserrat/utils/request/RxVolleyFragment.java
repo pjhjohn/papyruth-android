f49dc3b88d50e9e6be26e785d895d930afcba9a1
pjhjohn <pjhjohn@gmail.com>
2015-05-04 15:59:14 +0900

    [RxVolleyFragment] Fragment의 newInstance 의존성을 점점 줄여보는중.

---
+++ app/src/main/java/com/montserrat/utils/request/RxVolleyFragment.java
@@ -0,0 +1,59 @@
+package com.montserrat.utils.request;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.montserrat.controller.AppConst;
+
+import org.json.JSONObject;
+
+/**
+ * Created by pjhjohn on 2015-05-04.
+ * Boilerplate fragment that provides some utility functions for child classes.
+ */
+
+public abstract class RxVolleyFragment extends Fragment {
+    protected View vProgress, vContent;
+    private int transitionTime;
+    private boolean contentVisibleWhenProgress;
+
+    @Override
+    public void onAttach (Activity activity) {
+        super.onAttach(activity);
+        // TODO : Potential Context Bug when called from two different activities (Not Checked)
+        this.transitionTime = this.getResources().getInteger(android.R.integer.config_shortAnimTime);
+        this.contentVisibleWhenProgress = true;
+    }
+
+    protected void setContentVisibilityOnProgress (boolean visibleOnProgress) {
+        this.contentVisibleWhenProgress = visibleOnProgress;
+    }
+
+    private void showProgress (final boolean show) {
+        if (this.vProgress != null) {
+            this.vProgress.setVisibility(show ? View.VISIBLE : View.GONE);
+            this.vProgress.animate().setDuration(this.transitionTime).alpha(show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd (Animator animation) {
+                    RxVolleyFragment.this.vProgress.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        }
+        if (this.vContent != null && !this.contentVisibleWhenProgress) {
+            this.vContent.setVisibility(show ? View.GONE : View.VISIBLE);
+            this.vContent.animate().setDuration(this.transitionTime).alpha(show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd (Animator animation) {
+                    RxVolleyFragment.this.vContent.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+        }
+    }
+}
