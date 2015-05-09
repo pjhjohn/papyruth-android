04bc3c2850ff2707cf0c5c9b590b1b8483b0b87e
Shinseungsou <wwww432@lycos.co.kr>
2015-05-09 23:04:08 +0900

    EvaluationFragment Step2 Butterknife 추가. 내용 변경 불가능하도록 만듬

--- app/src/main/java/com/montserrat/app/fragment/main/EvaluationStep2Fragment.java
+++ app/src/main/java/com/montserrat/app/fragment/main/EvaluationStep2Fragment.java
@@ -3,14 +3,19 @@ package com.montserrat.app.fragment.main;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.montserrat.app.R;
 import com.montserrat.app.AppConst;
 import com.montserrat.utils.request.ClientFragment;
 import com.montserrat.utils.viewpager.ViewPagerController;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by pjhjohn on 2015-04-26.
@@ -18,6 +23,9 @@ import com.montserrat.utils.viewpager.ViewPagerController;
 
 public class EvaluationStep2Fragment extends Fragment {
     private ViewPagerController pagerController;
+
+    @InjectView(R.id.autotext_lecture) protected EditText vLecture;
+    @InjectView(R.id.autotext_professor) protected EditText vProfessor;
 
     @Override
     public void onAttach(Activity activity) {
@@ -30,7 +38,8 @@ public class EvaluationStep2Fragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_evaluation_step2, container, false);
 
         view.findViewById(R.id.btn_next).setOnClickListener(v -> this.pagerController.setCurrentPage(AppConst.ViewPager.Evaluation.EVALUATION_STEP3, true));
-
+        ButterKnife.inject(this, view);
+        vLecture.setFocusableInTouchMode(false);
         return view;
     }
 }
