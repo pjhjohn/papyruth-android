fc67e0293182a21d57666ef75b47d51e6c3a7e37
pjhjohn <pjhjohn@gmail.com>
2015-05-18 20:25:09 +0900

    Removed redundent features from model/User.java

--- app/src/main/java/com/montserrat/app/model/User.java
+++ app/src/main/java/com/montserrat/app/model/User.java
@@ -1,8 +1,5 @@
 package com.montserrat.app.model;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 /**
  * Created by pjhjohn on 2015-04-17.
  * Used for passing data during sign up process.
@@ -87,18 +84,10 @@ public class User {
         return this.university_name;
     }
 
-    public int getCompletionLevel() {
-        /* STEP 1 */
-        if(this.university_id == null) return 0;
-        /* STEP 2 */
-        if(this.realname == null || this.nickname == null || this.email == null || this.is_boy == null || this.entrance_year == null ) return 1;
-        return 2;
-    }
-
-    public void update(User.Data user) {
+    public void update(UserData user) {
         this.update(user, null);
     }
-    public void update(User.Data user, String access_token) {
+    public void update(UserData user, String access_token) {
         this.setEmail(user.email);
         this.setRealname(user.realname);
         this.setNickName(user.nickname);
@@ -108,49 +97,18 @@ public class User {
         this.setAccessToken(access_token);
     }
 
-    /**
-     * @param data data to assign
-     */
-    public void setData(JSONObject data) {
-        this.setAccessToken(data.optString("access_token", null));
-        try { this.realname      = data.getString("realname");   } catch (JSONException e) { e.printStackTrace(); }
-        try { this.nickname      = data.getString("nickname");   } catch (JSONException e) { e.printStackTrace(); }
-        try { this.email         = data.getString("email");   } catch (JSONException e) { e.printStackTrace(); }
-        try { this.university_id = data.getInt("university_id");   } catch (JSONException e) { e.printStackTrace(); }
-        try { this.entrance_year = data.getInt("entrance_year");   } catch (JSONException e) { e.printStackTrace(); }
-        try { this.is_boy        = data.getBoolean("is_boy");   } catch (JSONException e) { e.printStackTrace(); }
-    }
-
-    /**
-     * @return null if there exist an exception during assigning values
-     */
-    public JSONObject getData () {
-        JSONObject data = new JSONObject();
-        try {
-            if(this.realname      != null) data.put("realname"       , this.realname     );
-            if(this.nickname      != null) data.put("nickname"       , this.nickname     );
-            if(this.email         != null) data.put("email"          , this.email        );
-            if(this.university_id != null) data.put("university_id"  , this.university_id);
-            if(this.entrance_year != null) data.put("entrance_year"  , this.entrance_year);
-            if(this.is_boy        != null) data.put("is_boy"         , this.is_boy       );
-        } catch (JSONException e) {
-            data = null;
-        }
-        return data;
-    }
-
     @Override
     public String toString() {
         return String.format("<access_token : %s>\n <realname : %s>\n <nickname : %s>\n <email : %s>\n <university_id : %d>\n <entrance_year : %d>\n <is_boy : %b>\n", this.access_token, this.realname, this.nickname, this.email, this.university_id, this.entrance_year, this.is_boy);
     }
 
     public static class ResponseData {
-        public Data user;
+        public UserData user;
         public String access_token;
         public Boolean success;
     }
 
-    public static class Data {
+    private static class UserData {
         public Integer id;
         public Boolean is_boy;
         public Integer university_id;
