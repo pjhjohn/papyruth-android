882df4fe819acd98b536bfa39146c17501e29172
pjhjohn <pjhjohn@gmail.com>
2015-05-27 23:24:33 +0900

    Corrected typos in string resources. Added model for AutoComplete

---
+++ app/src/main/java/com/montserrat/app/model/AutoCompleteResponse.java
@@ -0,0 +1,12 @@
+package com.montserrat.app.model;
+
+/**
+ * Created by pjhjohn on 2015-05-27.
+ */
+public class AutoCompleteResponse {
+    String professor_name;
+    Integer professor_id;
+    String lecture_name;
+    Integer lecture_id;
+    PartialCourse course;
+}
