2a888bf0e74303d1601d1b66b2b91b430a2b2e06
pjhjohn <pjhjohn@gmail.com>
2015-06-26 18:05:59 +0900

    Model sync to API

--- app/src/main/java/com/montserrat/app/model/CommentData.java
+++ app/src/main/java/com/montserrat/app/model/CommentData.java
@@ -1,5 +1,7 @@
 package com.montserrat.app.model;
 
+import java.util.List;
+
 /**
  * Created by SSS on 2015-05-22.
  */
@@ -11,4 +13,9 @@ public class CommentData {
     public String created_at;
     public String updated_at;
     public String user_nickname;
+    public String up_vote_count;
+    public List<VoteData> up_votes;
+    public String down_vote_count;
+    public List<VoteData> down_votes;
+    public String avatar_url;
 }
