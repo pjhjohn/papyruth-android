4c7deb96452f919bc82f9d54fd548a71879bbb35
Shinseungsou <wwww432@lycos.co.kr>
2015-09-07 19:47:56 +0900

    [#106]  SignUpStepUnivFragment 에서 selected 되었을 때 배경 색깔이 변경되도록 적용

--- app/src/main/java/com/montserrat/app/fragment/auth/SignUpStepUnivFragment.java
+++ app/src/main/java/com/montserrat/app/fragment/auth/SignUpStepUnivFragment.java
@@ -2,6 +2,7 @@ package com.montserrat.app.fragment.auth;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -15,12 +16,15 @@ import com.montserrat.app.R;
 import com.montserrat.app.activity.AuthActivity;
 import com.montserrat.app.model.UniversityData;
 import com.montserrat.app.model.unique.Signup;
+import com.montserrat.app.model.unique.User;
 import com.montserrat.app.recyclerview.adapter.UniversityAdapter;
 import com.montserrat.utils.support.fab.FloatingActionControl;
 import com.montserrat.utils.support.retrofit.RetrofitApi;
 import com.montserrat.utils.view.fragment.RecyclerViewFragment;
 import com.montserrat.utils.view.viewpager.OnPageFocus;
+import com.montserrat.utils.view.viewpager.OnPageUnfocus;
 import com.montserrat.utils.view.viewpager.ViewPagerController;
+import com.squareup.picasso.Picasso;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -34,6 +38,11 @@ import rx.subscriptions.CompositeSubscription;
 
 public class SignUpStepUnivFragment extends RecyclerViewFragment<UniversityAdapter, UniversityData> implements OnPageFocus {
     private ViewPagerController pagerController;
+
+    @InjectView (R.id.signup_univ_recyclerview) protected RecyclerView universityList;
+    private CompositeSubscription subscriptions;
+
+
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
@@ -45,15 +54,13 @@ public class SignUpStepUnivFragment extends RecyclerViewFragment<UniversityAdapt
         this.pagerController = null;
     }
 
-    @InjectView (R.id.signup_univ_recyclerview) protected RecyclerView universityList;
-    private CompositeSubscription subscriptions;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_signup_univ, container, false);
         ButterKnife.inject(this, view);
         this.subscriptions = new CompositeSubscription();
         this.setupRecyclerView(this.universityList);
+
 
         this.subscriptions.add(
             RetrofitApi.getInstance().universities()
@@ -81,7 +88,19 @@ public class SignUpStepUnivFragment extends RecyclerViewFragment<UniversityAdapt
         ((AuthActivity)this.getActivity()).signUpStep(0);
         FloatingActionControl.getInstance().hide(true);
         ((InputMethodManager)this.getActivity().getBaseContext().getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(this.universityList.getWindowToken(), 2);
+        if(Signup.getInstance().getUniversity_id() != null){
+            this.universityList.getChildAt(getUniversityPosition()).setBackgroundColor(getResources().getColor(R.color.selected_gray));
+//            this.universityList.getChildAt(getUniversityPosition()).setAlpha((float) 0.4);
+        }
+    }
 
+
+    public int getUniversityPosition(){
+        for(int i = 0; i < items.size(); i++){
+            if(items.get(i).id == Signup.getInstance().getUniversity_id())
+                return i;
+        }
+        return -1;
     }
 
     @Override
@@ -96,8 +115,13 @@ public class SignUpStepUnivFragment extends RecyclerViewFragment<UniversityAdapt
 
     @Override
     public void onRecyclerViewItemClick(View view, int position) {
+        if(Signup.getInstance().getUniversity_id() != null) {
+            this.universityList.getChildAt(getUniversityPosition()).setBackgroundColor(getResources().getColor(R.color.transparent));
+//            this.universityList.getChildAt(getUniversityPosition()).setAlpha((float)1.0);
+        }
         Signup.getInstance().setUniversity_id(this.items.get(position).id);
         Signup.getInstance().setImage_url(this.items.get(position).image_url);
         this.pagerController.setCurrentPage(AppConst.ViewPager.Auth.SIGNUP_STEP1, true);
     }
+
 }
