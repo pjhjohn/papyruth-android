afa0c90088ab742b4370121259122daf1bd19732
Shinseungsou <wwww432@lycos.co.kr>
2015-10-06 23:41:37 +0900

    [closing #142] onResume 때 최신 강의평만을 가져오도록 수정.
    onResume에서 무조건 sinceId를 초기화하도록 설정.
    startWith 옵션이 처음 resume 되었을 때 Api를 2번 호출하게 하여 없앰.
    부수적으로 sinceId와 maxId를 for문 없이 가져오도록 알고리즘을 변경했는데 이 알고리즘은 Response로 받은 EvaluationList의 id가 순차적이라는 전제 하에 만들어짐.
    이에 대해 비교해볼 수 있도록 코드를 주석처리하여 남김.

--- app/src/main/java/com/montserrat/app/fragment/main/HomeFragment.java
+++ app/src/main/java/com/montserrat/app/fragment/main/HomeFragment.java
@@ -1,6 +1,9 @@
 package com.montserrat.app.fragment.main;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.montserrat.app.AppConst;
 import com.montserrat.app.R;
@@ -22,11 +25,17 @@ import timber.log.Timber;
 
 public class HomeFragment extends CommonRecyclerViewFragment<EvaluationItemsDetailAdapter, EvaluationData> {
 
-    private Integer sinceId = null, maxId = null;
+    private Integer sinceId = null, maxId = null, firstId = null, size = null;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 
     @Override
     public void onResume() {
         super.onResume();
+        sinceId = null;
         toolbar.setTitle(R.string.toolbar_title_home);
         ToolbarUtil.getColorTransitionAnimator(toolbar, AppConst.COLOR_POINT_CLARITY).start();
         this.subscriptions.add(super.getRefreshObservable(this.swipeRefresh)
@@ -43,34 +52,56 @@ public class HomeFragment extends CommonRecyclerViewFragment<EvaluationItemsDeta
                 if (evaluations != null)
                     this.items.addAll(evaluations);
                 this.adapter.notifyDataSetChanged();
+                size = items.size();
             })
         );
 
         this.subscriptions.add(super.getRecyclerViewScrollObservable(this.recyclerView, this.toolbar, true)
-            .startWith((Boolean) null)
             .filter(passIfNull -> passIfNull == null && this.progress.getVisibility() != View.VISIBLE)
             .observeOn(AndroidSchedulers.mainThread())
             .flatMap(unused -> {
                 this.progress.setVisibility(View.VISIBLE);
                 // TODO : handle the case for max_id == 0 : prefer not to request to server
-                return RetrofitApi.getInstance().get_evaluations(User.getInstance().getAccessToken(), User.getInstance().getUniversityId(), null, sinceId == null ? null : sinceId - 1, null, null);
+                return RetrofitApi.getInstance().get_evaluations(
+                    User.getInstance().getAccessToken(),
+                    User.getInstance().getUniversityId(),
+                    null,
+                    sinceId == null ? null : sinceId - 1,
+                    null,
+                    null
+                );
             })
             .map(evaluations -> evaluations.evaluations)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(evaluations -> {
+
                 this.progress.setVisibility(View.GONE);
-                if (evaluations != null) this.items.addAll(evaluations);
-                this.adapter.notifyDataSetChanged();
-                // TODO : Implement Better Algorithm
-                for (EvaluationData evaluation : evaluations) {
-                    final int id = evaluation.id;
-                    if (maxId == null) maxId = id;
-                    else if (maxId < id) maxId = id;
-                    if (sinceId == null) sinceId = id;
-                    else if (sinceId > id) sinceId = id;
+                if (evaluations != null) {
+                    this.items.addAll(evaluations);
+
+                    // TODO : Compare with below TODO Algorithm.
+                    if(evaluations.get(evaluations.size()-1).id < evaluations.get(0).id) {
+                        sinceId = evaluations.get(evaluations.size()-1).id;
+                        maxId = evaluations.get(0).id;
+                    }else{
+                        sinceId = evaluations.get(0).id;
+                        maxId = evaluations.get(evaluations.size()-1).id;
+                    }
                 }
-            })
+                this.adapter.notifyDataSetChanged();
+                if(firstId == null)
+                    firstId = this.items.get(0).id;
+                size = items.size();
+                // TODO : Implement Better Algorithm.
+//                for (EvaluationData evaluation : evaluations) {
+//                    final int id = evaluation.id;
+//                    if (maxId == null) maxId = id;
+//                    else if (maxId < id) maxId = id;
+//                    if (sinceId == null) sinceId = id;
+//                    else if (sinceId > id) sinceId = id;
+//                }
+            }, error -> error.printStackTrace())
         );
     }
 
@@ -101,6 +132,4 @@ public class HomeFragment extends CommonRecyclerViewFragment<EvaluationItemsDeta
         FloatingActionControl.getInstance().setControl(R.layout.fam_home).show(true, 200, TimeUnit.MILLISECONDS);
         FloatingActionControl.clicks(R.id.fab_new_evaluation).subscribe(unused -> navigator.navigate(EvaluationStep1Fragment.class, true, FragmentNavigator.AnimatorType.SLIDE_TO_DOWN));
     }
-
-
 }
