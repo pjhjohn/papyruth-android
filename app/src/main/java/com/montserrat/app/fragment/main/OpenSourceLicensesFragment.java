eb0b58e8fa7082a4c8a8418cb27bde1cb6b5ccd1
pjhjohn <pjhjohn@gmail.com>
2015-11-08 18:29:33 +0900

    Added license for ColorFilterTransformation

--- app/src/main/java/com/montserrat/app/fragment/main/OpenSourceLicensesFragment.java
+++ app/src/main/java/com/montserrat/app/fragment/main/OpenSourceLicensesFragment.java
@@ -491,6 +491,24 @@ public class OpenSourceLicensesFragment extends RecyclerViewFragment<OpenSourceL
             "https://github.com/flavienlaurent/PanningView/",
             R.drawable.ic_github
         ));
+        licenses.add(new OpenSourceLicenseData(
+            "Picasso Transformations",
+            "Copyright 2015 Wasabeef\n" +
+            "\n" +
+            "Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
+            "you may not use this file except in compliance with the License.\n" +
+            "You may obtain a copy of the License at\n" +
+            "\n" +
+            "   http://www.apache.org/licenses/LICENSE-2.0\n" +
+            "\n" +
+            "Unless required by applicable law or agreed to in writing, software\n" +
+            "distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
+            "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
+            "See the License for the specific language governing permissions and\n" +
+            "limitations under the License.",
+            "https://github.com/wasabeef/picasso-transformations",
+            R.drawable.ic_github
+        ));
         return licenses;
     }
 }
