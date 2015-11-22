f81f6a41ffb360a91d7c23fd41220f3b365838b6
pjhjohn <pjhjohn@gmail.com>
2015-11-23 01:26:27 +0900

    Designed CourseItemViewHolder layout. Added missing string resource for GA

--- app/src/main/java/com/papyruth/android/recyclerview/viewholder/CourseItemViewHolder.java
+++ app/src/main/java/com/papyruth/android/recyclerview/viewholder/CourseItemViewHolder.java
@@ -13,9 +13,11 @@ import android.widget.LinearLayout;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.devspark.robototextview.widget.RobotoTextView;
 import com.papyruth.android.R;
 import com.papyruth.android.model.CourseData;
 import com.papyruth.utils.support.picasso.CircleTransformation;
+import com.papyruth.utils.support.picasso.ColorFilterTransformation;
 import com.papyruth.utils.view.Hashtag;
 import com.papyruth.utils.view.recycler.RecyclerViewItemClickListener;
 import com.squareup.picasso.Picasso;
@@ -27,15 +29,16 @@ import butterknife.InjectView;
  * Created by pjhjohn on 2015-06-29.
  */
 public class CourseItemViewHolder extends RecyclerView.ViewHolder {
-    @InjectView(R.id.course_item_category)           protected TextView mCategory;
-    @InjectView(R.id.course_item_lecture)            protected TextView mLecture;
-    @InjectView(R.id.course_item_professor)          protected TextView mProfessor;
     @InjectView(R.id.course_item_professor_image)    protected ImageView mProfessorImage;
-    @InjectView(R.id.course_item_point_overall_star) protected RatingBar mPointOverallStar;
-    @InjectView(R.id.course_item_point_overall_text) protected TextView mPointOverallText;
+    @InjectView(R.id.course_item_lecture)            protected TextView mLecture;
+    @InjectView(R.id.course_item_category)           protected TextView mCategory;
+    @InjectView(R.id.course_item_professor)          protected TextView mProfessor;
+    @InjectView(R.id.course_item_overall_label)      protected TextView mLabelOverall;
+    @InjectView(R.id.course_item_overall_point)      protected RobotoTextView mPointOverall;
+    @InjectView(R.id.course_item_overall_ratingbar)  protected RatingBar mRatingBarOverall;
     @InjectView(R.id.course_item_hashtags)           protected LinearLayout mHashtags;
-    @InjectView(R.id.course_item_evaluator_icon)     protected ImageView mEvaluatorIcon;
-    @InjectView(R.id.course_item_evaluator_count)    protected TextView mEvaluatorCount;
+    @InjectView(R.id.course_item_evaluation_icon)    protected ImageView mEvaluationIcon;
+    @InjectView(R.id.course_item_evaluation_count)   protected TextView mEvaluationCount;
     private final Context mContext;
     private final Resources mResources;
     public CourseItemViewHolder(View view, RecyclerViewItemClickListener listener) {
@@ -43,33 +46,20 @@ public class CourseItemViewHolder extends RecyclerView.ViewHolder {
         ButterKnife.inject(this, view);
         mContext = view.getContext();
         mResources = mContext.getResources();
-        view.setOnClickListener(v -> listener.onRecyclerViewItemClick(v, super.getAdapterPosition() - 1));
         mLecture.setPaintFlags(mLecture.getPaintFlags() | Paint.FAKE_BOLD_TEXT_FLAG);
         mCategory.setPaintFlags(mCategory.getPaintFlags() | Paint.FAKE_BOLD_TEXT_FLAG);
-        mCategory.setTextColor(mResources.getColor(R.color.colorchip_green_highlight));
-        mProfessor.setTextColor(mResources.getColor(R.color.colorchip_green_highlight));
-    }
-
-    private void setRatingBarColor(RatingBar rating, int color) {
-        LayerDrawable stars = (LayerDrawable) rating.getProgressDrawable();
-        for(int i = 0; i < 3; i ++) stars.getDrawable(i).setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
-        mPointOverallText.setTextColor(color);
-    }
-
-    private void setPointRating(RatingBar rating, Integer point_sum, Integer count) {
-        Float rating_value = count == null || count <= 0 ? null : point_sum == null ? null : (float)point_sum / (float)count / 2f;
-        if(rating_value == null || rating_value < 0) setRatingBarColor(rating, mResources.getColor(R.color.inactive));
-        else setRatingBarColor(rating, mResources.getColor(rating_value >= 8 ? R.color.point_high : R.color.point_low));
-        rating.setRating(rating_value == null || rating_value < 0 ? 5.0f : rating_value);
-        mPointOverallText.setText(rating_value == null || rating_value < 0 ? "0" : String.format("%.1f", rating_value));
+        mLabelOverall.setPaintFlags(mLabelOverall.getPaintFlags() | Paint.FAKE_BOLD_TEXT_FLAG);
+        view.setOnClickListener(v -> listener.onRecyclerViewItemClick(v, super.getAdapterPosition() - 1));
     }
 
     public void bind(CourseData course) {
-        mCategory.setText(course.category);
+        Picasso.with(mContext).load(course.professor_photo_url).transform(new CircleTransformation()).into(mProfessorImage);
         mLecture.setText(course.name);
         mProfessor.setText(Html.fromHtml(String.format("%s<strong>%s</strong>%s", mResources.getString(R.string.professor_prefix), course.professor_name, mResources.getString(R.string.professor_postfix))));
-        Picasso.with(mContext).load(course.professor_photo_url).transform(new CircleTransformation()).into(mProfessorImage);
-        setPointRating(mPointOverallStar, course.point_overall, course.evaluation_count);
+        setCategoryProfessorColor(mCategory, mProfessor, course.category);
+        mLabelOverall.setText(R.string.label_point_overall);
+        if (course.point_overall != null && course.evaluation_count != null && course.evaluation_count != 0) setPointRating(mLabelOverall, mRatingBarOverall, mPointOverall, (float) course.point_overall / (float) course.evaluation_count);
+        else setPointRating(mLabelOverall, mRatingBarOverall, mPointOverall, -1);
         mHashtags.removeAllViews();
         if(course.hashtags!=null) mHashtags.post(() -> {
             float totalWidth = 0;
@@ -82,6 +72,34 @@ public class CourseItemViewHolder extends RecyclerView.ViewHolder {
                 totalWidth += width;
             }
         });
-        mEvaluatorCount.setText(course.evaluation_count == null || course.evaluation_count < 0 ? "N/A" : String.valueOf(course.evaluation_count));
+        Picasso.with(mContext).load(R.drawable.ic_light_people).transform(new ColorFilterTransformation(mResources.getColor(R.color.icon_material))).into(mEvaluationIcon);
+        mEvaluationCount.setText(pointInRange(course.evaluation_count) ? String.valueOf(course.evaluation_count) : "N/A");
+    }
+
+    private void setCategoryProfessorColor(TextView category, TextView professor, String value) {
+        int color;
+        if(value == null || value.isEmpty()) {
+            color = mResources.getColor(R.color.lecture_type_etc);
+            category.setText(R.string.lecture_type_etc);
+        } else {
+            color = mResources.getColor(R.color.lecture_type_major);
+            category.setText(R.string.lecture_type_major);
+        }
+        category.setTextColor(color);
+        category.getBackground().setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
+        professor.setTextColor(color);
+    }
+
+    private void setPointRating(TextView label, RatingBar ratingbar, TextView point, float value) {
+        final int pointColor = mResources.getColor(pointInRange(value)? ( value>=8?R.color.point_high:R.color.point_low ) : R.color.point_none);
+        label.setTextColor(pointColor);
+        point.setTextColor(pointColor);
+        point.setText(pointInRange(value) ? (value >= 10 ? "10" : String.format("%.1f", value)) : "N/A");
+        for(int i = 0; i < 3; i ++) ((LayerDrawable) ratingbar.getProgressDrawable()).getDrawable(i).setColorFilter(pointColor, PorterDuff.Mode.SRC_ATOP);
+        ratingbar.setRating(pointInRange(value) ? value / 2f : 5.0f);
+    }
+
+    private boolean pointInRange(float point) {
+        return 0 <= point && point <= 10;
     }
 }
