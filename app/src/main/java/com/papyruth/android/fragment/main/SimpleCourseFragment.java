4f820f7d8aebe51dc5698b7b960e11470ca896c6
Shinseungsou <wwww432@lycos.co.kr>
2015-12-07 21:59:11 +0900

    [#187] SimpleCourseFragment Adapter 개선 완료

--- app/src/main/java/com/papyruth/android/fragment/main/SimpleCourseFragment.java
+++ app/src/main/java/com/papyruth/android/fragment/main/SimpleCourseFragment.java
@@ -1,8 +1,9 @@
 package com.papyruth.android.fragment.main;
 
 import android.app.Activity;
-import android.content.Context;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,8 +11,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Toast;
+import android.widget.FrameLayout;
 
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
@@ -19,28 +19,24 @@ import com.papyruth.android.AppConst;
 import com.papyruth.android.PapyruthApplication;
 import com.papyruth.android.R;
 import com.papyruth.android.activity.MainActivity;
-import com.papyruth.android.model.Candidate;
 import com.papyruth.android.model.CourseData;
 import com.papyruth.android.model.unique.Course;
 import com.papyruth.android.model.unique.User;
-import com.papyruth.android.recyclerview.adapter.CourseItemsAdapter;
+import com.papyruth.android.recyclerview.adapter.SimpleCourseAdapter;
 import com.papyruth.support.opensource.fab.FloatingActionControl;
 import com.papyruth.support.opensource.materialdialog.AlertDialog;
-import com.papyruth.support.opensource.retrofit.apis.Api;
 import com.papyruth.support.utility.error.ErrorHandler;
-import com.papyruth.support.utility.fragment.RecyclerViewFragment;
 import com.papyruth.support.utility.helper.StatusBarHelper;
 import com.papyruth.support.utility.helper.ToolbarHelper;
 import com.papyruth.support.utility.navigator.Navigator;
+import com.papyruth.support.utility.recyclerview.RecyclerViewItemObjectClickListener;
 import com.papyruth.support.utility.search.SearchToolbar;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
 import rx.subscriptions.CompositeSubscription;
 
 /**
@@ -48,13 +44,15 @@ import rx.subscriptions.CompositeSubscription;
  * TODO : should be able to expand when clicking recyclerview item to show evaluation data in detail
  */
 
-public class SimpleCourseFragment extends RecyclerViewFragment<CourseItemsAdapter, CourseData> {
-    private Navigator navigator;
+public class SimpleCourseFragment extends Fragment implements RecyclerViewItemObjectClickListener {
+    private Navigator mNavigator;
+    private Tracker mTracker;
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-        this.navigator = (Navigator) activity;
+        this.mNavigator = (Navigator) activity;
+        mTracker = ((PapyruthApplication) getActivity().getApplication()).getTracker();
     }
 
     @Override
@@ -62,57 +60,38 @@ public class SimpleCourseFragment extends RecyclerViewFragment<CourseItemsAdapte
         super.onDetach();
         SearchToolbar.getInstance().setItemClickListener(null);
     }
+    @InjectView(R.id.common_swipe_refresh) protected SwipeRefreshLayout mSwipeRefresh;
+    @InjectView(R.id.common_recycler_view) protected RecyclerView mRecyclerView;
+    @InjectView(R.id.common_empty_state)   protected FrameLayout mEmptyState;
+    private CompositeSubscription mCompositeSubscription;
+    private Toolbar mToolbar;
+    private SimpleCourseAdapter mAdapter;
 
-    @InjectView(R.id.recyclerview) protected RecyclerView recycler;
-    @InjectView(R.id.swipe) protected SwipeRefreshLayout refresh;
-    @InjectView(R.id.material_progress_large) protected View progress;
-    private CompositeSubscription subscriptions;
-    private Toolbar toolbar;
-    private Tracker mTracker;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mTracker = ((PapyruthApplication) getActivity().getApplication()).getTracker();
-    }
-
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_course_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_common_recyclerview, container, false);
         ButterKnife.inject(this, view);
-        this.subscriptions = new CompositeSubscription();
-        this.toolbar = (Toolbar) this.getActivity().findViewById(R.id.toolbar);
-        this.refresh.setEnabled(true);
+        mCompositeSubscription = new CompositeSubscription();
 
-        this.setupRecyclerView(recycler);
-        ((InputMethodManager) this.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(view.getWindowToken(), 2);
+        mToolbar = (Toolbar) this.getActivity().findViewById(R.id.toolbar);
+        mSwipeRefresh.setEnabled(false);
+
+        mAdapter = new SimpleCourseAdapter(getActivity(), mEmptyState, this);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setAdapter(mAdapter);
+
         return view;
-    }
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        this.setupSwipeRefresh(this.refresh);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        FloatingActionControl.getInstance().closeMenuButton(true);
         ButterKnife.reset(this);
-        if(this.subscriptions!=null && !this.subscriptions.isUnsubscribed()) this.subscriptions.unsubscribe();
         SearchToolbar.getInstance().setOnSearchByQueryListener(null).setItemClickListener(null);
-    }
-
-    @Override
-    protected CourseItemsAdapter getAdapter () {
-        if(this.adapter != null)
-            return adapter;
-        return new CourseItemsAdapter(this.items, this, R.string.no_data_search);
-    }
-
-    @Override
-    protected RecyclerView.LayoutManager getRecyclerViewLayoutManager () {
-        return new LinearLayoutManager(this.getActivity());
+        if(mCompositeSubscription == null || mCompositeSubscription.isUnsubscribed()) return;
+        mCompositeSubscription.unsubscribe();
     }
 
     @Override
@@ -120,81 +99,31 @@ public class SimpleCourseFragment extends RecyclerViewFragment<CourseItemsAdapte
         super.onResume();
         mTracker.setScreenName(getResources().getString(R.string.ga_fragment_main_search_result));
         mTracker.send(new HitBuilders.ScreenViewBuilder().build());
-        this.toolbar.setTitle(R.string.toolbar_search);
-        SearchToolbar.getInstance().setItemClickListener((v, position) -> {
-            SearchToolbar.getInstance().setSelectedCandidate(position);
-            SearchToolbar.getInstance().addToHistory(SearchToolbar.getInstance().getSelectedCandidate());
-            this.getSearchResult();
-        }).setOnSearchByQueryListener(this::getSearchResult);
-
-        ToolbarHelper.getColorTransitionAnimator(toolbar, R.color.toolbar_red).start();
+        this.mToolbar.setTitle(R.string.toolbar_search);
+        ToolbarHelper.getColorTransitionAnimator(mToolbar, R.color.toolbar_red).start();
         StatusBarHelper.changeColorTo(getActivity(), R.color.status_bar_red);
-        FloatingActionControl.getInstance().setControl(R.layout.fab_normal_new_evaluation_red).show(true, 200, TimeUnit.MILLISECONDS);
-        FloatingActionControl.clicks().observeOn(AndroidSchedulers.mainThread()).subscribe(
-            unused -> navigator.navigate(EvaluationStep1Fragment.class, true),
-            error -> ErrorHandler.handle(error, this)
-        );
         ((MainActivity) getActivity()).setMenuItemVisibility(AppConst.Menu.SETTING, false);
         ((MainActivity) getActivity()).setMenuItemVisibility(AppConst.Menu.SEARCH, true);
 
-        this.subscriptions.add(
-            this.getRefreshObservable(this.refresh)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                    courses -> {
-                        this.refresh.setRefreshing(false);
-                        this.getSearchResult();
-                    },
-                    error -> ErrorHandler.handle(error, this)
-                )
+        FloatingActionControl.getInstance().setControl(R.layout.fab_normal_new_evaluation_red).show(true, 200, TimeUnit.MILLISECONDS);
+        FloatingActionControl.clicks().observeOn(AndroidSchedulers.mainThread()).subscribe(
+            unused -> mNavigator.navigate(EvaluationStep1Fragment.class, true),
+            error -> ErrorHandler.handle(error, this)
         );
-//        toolbarSearch.searchCourse();
-        this.getSearchResult();
-    }
-    private void getSearchResult(){
-        Candidate candidate = SearchToolbar.getInstance().getSelectedCandidate();
-        String query = SearchToolbar.getInstance().getSelectedQuery();
-        this.subscriptions.add(
-            Api.papyruth().search_search(
-                User.getInstance().getAccessToken(),
-                User.getInstance().getUniversityId(),
-                candidate.lecture_id,
-                candidate.professor_id,
-                query
-            )
-                .map(response -> response.courses)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(this::notifyDataSetChanged, error -> ErrorHandler.handle(error, this))
-        );
-    }
-
-    public void notifyDataSetChanged(List<CourseData> courseDatas){
-        this.items.clear();
-        this.items.addAll(courseDatas);
-        this.adapter.notifyDataSetChanged();
+        mAdapter.loadSearchResult();
     }
 
     @Override
-    public void onRecyclerViewItemClick(View view, int position) {
+    public void onRecyclerViewItemObjectClick(View view, Object object) {
         if(User.getInstance().needEmailConfirmed()){
-            AlertDialog.show(getActivity(), navigator, AlertDialog.Type.NEED_CONFIRMATION);
+            AlertDialog.show(getActivity(), mNavigator, AlertDialog.Type.NEED_CONFIRMATION);
             return;
         }
         if(User.getInstance().needMoreEvaluation()) {
-            AlertDialog.show(getActivity(), navigator, AlertDialog.Type.EVALUATION_MANDATORY);
+            AlertDialog.show(getActivity(), mNavigator, AlertDialog.Type.EVALUATION_MANDATORY);
             return;
         }
-        if(this.items.size() -1 < position){
-            Toast.makeText(getActivity().getBaseContext(),"please wait for loading", Toast.LENGTH_LONG).show();
-            return;
-        }
-        if(this.items.get(position).id == null){
-            return;
-        }
-
-        Course.getInstance().update(this.items.get(position));
-        this.navigator.navigate(CourseFragment.class, true);
+        Course.getInstance().update(((CourseData) object));
+        this.mNavigator.navigate(CourseFragment.class, true);
     }
 }
