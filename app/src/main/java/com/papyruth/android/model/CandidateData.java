18b298e7b68a06790765b983458863cc0411b017
Shinseungsou <wwww432@lycos.co.kr>
2015-12-21 23:23:14 +0900

    (API 변경 사항 반영) CandidateData에 course_id 추가

--- app/src/main/java/com/papyruth/android/model/CandidateData.java
+++ app/src/main/java/com/papyruth/android/model/CandidateData.java
@@ -4,26 +4,29 @@ package com.papyruth.android.model;
  * Created by pjhjohn on 2015-05-27.
  */
 public class CandidateData {
+    public Integer course_id;
     public String professor_name;
     public Integer professor_id;
     public String lecture_name;
     public Integer lecture_id;
 
     public CandidateData() {
-        this.professor_id = null;
+        this.course_id      = null;
+        this.professor_id   = null;
         this.professor_name = null;
-        this.lecture_id = null;
-        this.lecture_name = null;
+        this.lecture_id     = null;
+        this.lecture_name   = null;
     }
 
     public CandidateData(String lecture_name, Integer lecture_id, String professor_name, Integer professor_id, CourseData course){
         this.professor_name = professor_name;
-        this.professor_id = professor_id;
-        this.lecture_name = lecture_name;
-        this.lecture_id = lecture_id;
+        this.professor_id   = professor_id;
+        this.lecture_name   = lecture_name;
+        this.lecture_id     = lecture_id;
     }
 
     public void clear() {
+        this.course_id = null;
         this.professor_id = null;
         this.professor_name = null;
         this.lecture_id = null;
@@ -32,7 +35,7 @@ public class CandidateData {
 
     @Override
     public String toString() {
-        return String.format("professor : id-<%s>, name-<%s>  lecture : id-<%s>, name-<%s>", professor_id, professor_name, lecture_id, lecture_name);
+        return String.format("course : id - <%s>  professor : id-<%s>, name-<%s>  lecture : id-<%s>, name-<%s>", course_id, professor_id, professor_name, lecture_id, lecture_name);
     }
 
     @Override
@@ -40,13 +43,15 @@ public class CandidateData {
         if(this == object) return true;
         if(object == null || getClass() != object.getClass()) return false;
         CandidateData candidate = (CandidateData) object;
-        return (this.lecture_id!= null && this.lecture_id.equals(candidate.lecture_id))
+        return (course_id != null && course_id.equals(candidate.course_id))
+            || (this.lecture_id!= null && this.lecture_id.equals(candidate.lecture_id))
             || (this.professor_id!=null && this.professor_id.equals(candidate.professor_id));
     }
 
     @Override
     public int hashCode() {
         int hash = 7;
+        hash = (37 * hash) + (null == course_id    ? 0 :    course_id.hashCode());
         hash = (37 * hash) + (null == lecture_id   ? 0 :   lecture_id.hashCode());
         hash = (37 * hash) + (null == professor_id ? 0 : professor_id.hashCode());
         return hash;
