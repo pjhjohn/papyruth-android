5191abd06fc6db0dfaf5b7c58aff44d3ab32b4c2
pjhjohn <pjhjohn@gmail.com>
2016-01-08 00:48:55 +0900

    Removed PanningView

--- app/src/main/java/com/papyruth/android/recyclerview/adapter/OpenSourceLicensesAdapter.java
+++ app/src/main/java/com/papyruth/android/recyclerview/adapter/OpenSourceLicensesAdapter.java
@@ -452,24 +452,6 @@ public class OpenSourceLicensesAdapter extends RecyclerView.Adapter<RecyclerView
             "https://github.com/orfjackal/retrolambda",
             R.drawable.ic_github_24dp));
         licenses.add(new OpenSourceLicenseData(
-            "PanningView",
-            "Copyright 2013 Flavien Laurent\n" +
-                "\n" +
-                "Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
-                "you may not use this file except in compliance with the License.\n" +
-                "You may obtain a copy of the License at\n" +
-                "\n" +
-                "   http://www.apache.org/licenses/LICENSE-2.0\n" +
-                "\n" +
-                "Unless required by applicable law or agreed to in writing, software\n" +
-                "distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
-                "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
-                "See the License for the specific language governing permissions and\n" +
-                "limitations under the License.",
-            "https://github.com/flavienlaurent/PanningView/",
-            R.drawable.ic_github_24dp
-        ));
-        licenses.add(new OpenSourceLicenseData(
             "Picasso Transformations",
             "Copyright 2015 Wasabeef\n" +
                 "\n" +
